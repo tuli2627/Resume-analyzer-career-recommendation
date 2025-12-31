847192c14ba6bff830d26de72f69305f5ae1d0d7
tuli2627 <palpoulami94@gmail.com>
2025-12-31 19:04:53 +0530

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def resume_analyze():
 
 # -------------------- RUN APP --------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=5000)
